174a691cfa51fdbf0836a3247de9cfbe9255ca52
robbe.luo <robbe.luo@blacklake.cn>
2021-01-07 18:27:22 +0800

    init project

--- android/src/main/java/com/robbe/lib/ReactNativeFocusViewManager.java
+++ android/src/main/java/com/robbe/lib/ReactNativeFocusViewManager.java
@@ -3,12 +3,12 @@ package com.robbe.lib;
 import android.util.Log;
 import android.view.ViewGroup;
 
-import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.annotations.ReactProp;
+import com.facebook.react.views.view.ReactClippingViewManager;
 import com.facebook.react.views.view.ReactViewGroup;
 
-public class ReactNativeFocusViewManager extends SimpleViewManager<ReactViewGroup> {
+public class ReactNativeFocusViewManager extends ReactClippingViewManager<ReactViewGroup> {
 
     public static final String REACT_CLASS = "ReactNativeFocusView";
 
